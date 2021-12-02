424f67db02aee5c984bf0a752d4470b9177328d6
Udit Saini <udisaini@publicisgroupe.net>
2021-12-02 16:07:37 +0530

    aa

--- src/components/OrderManagement/Order.js
+++ src/components/OrderManagement/Order.js
@@ -15,45 +15,46 @@ const Order = (props) => {
   }
 
   return (
-        
-        <div className="ui cards align center">
-          <div className="card">
-            <div className="content">
-              <a className="header">ORDER ID: {props.order.orderId}</a>
-              <div class="meta">
-                <span className="date">Name: {props.order.nameCustomer}</span>
+        <div className="cardClass">
+          <div className="ui cards align center">
+            <div className="card">
+              <div className="content">
+                <a className="header">ORDER ID: {props.order.orderId}</a>
+                <div class="meta">
+                  <span className="date">Name: {props.order.nameCustomer}</span>
+                  <br/>
+                  <span className="date">Email Id: {props.order.email}</span>
+                  <br/>
+                  <span className="date">Mobile No: {props.order.mobileNumber}</span>
+                </div>
+                <div className="description">
+                Shipping Address: {props.order.shippingAddress}
                 <br/>
-                <span className="date">Email Id: {props.order.email}</span>
+                Billing Address: {props.order.billingAddress}
                 <br/>
-                <span className="date">Mobile No: {props.order.mobileNumber}</span>
+                Order Date: {props.order.orderDate}
+                </div>
               </div>
-              <div className="description">
-              Shipping Address: {props.order.shippingAddress}
-              <br/>
-              Billing Address: {props.order.billingAddress}
-              <br/>
-              Order Date: {props.order.orderDate}
+              <div class="extra content">
+              {props.order.paymentStatus==true?
+                <a>
+              <CardTitle className="cardTitle">Payment Status: Prepaid</CardTitle>
+                </a>:
+                <a>
+                <CardTitle className="cardTitle">Payment Status: Postpaid</CardTitle>
+                </a>}
               </div>
-            </div>
-            <div class="extra content">
-            {props.order.paymentStatus==true?
-              <a>
-            <CardTitle className="cardTitle">Payment Status: Prepaid</CardTitle>
-              </a>:
-              <a>
-              <CardTitle className="cardTitle">Payment Status: Postpaid</CardTitle>
-              </a>}
-            </div>
-            <div className="extra content">
-              <div className="ui two buttons">
-                <div className="positive ui button" onClick={editB} color="primary">UPDATE</div>
-                {/* <Button onClick={()=>addP(o)} color="primary" >ADD</Button> */}
+              <div className="extra content">
+                <div className="ui two buttons">
+                  <div className="positive ui button" onClick={editB} color="primary">UPDATE</div>
+                  {/* <Button onClick={()=>addP(o)} color="primary" >ADD</Button> */}
 
-                <div className="negative ui button" >TRACK ORDER</div>
-                {/* <Button color="danger" onClick={()=>deleteP(o)}>Delete</Button> */}
+                  <div className="negative ui button" >TRACK ORDER</div>
+                  {/* <Button color="danger" onClick={()=>deleteP(o)}>Delete</Button> */}
+                </div>
               </div>
-            </div>
-          </div>    
+            </div>    
+          </div>
         </div>
   )
 }
